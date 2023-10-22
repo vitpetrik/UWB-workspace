57d973dc52a82c4828e3276c1304d670f8b39ad8
Vit Petrik <petrivi2@fel.cvut.cz>
2023-10-22 14:10:51 +0200

    Finally successfull experiment

--- simulation/start_dry.sh
+++ simulation/start_dry.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-if_is_link=`readlink $0`
-command_name=$0
-if [[ if_is_link ]]; then
-  command_name=$if_is_link
-fi
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+cd "$SCRIPTPATH"
 
-MY_PATH=`dirname "$if_is_link"`
-MY_PATH=`( cd "$MY_PATH" && pwd )`
-cd $MY_PATH
-
-# remove the old link
-rm .tmuxinator.yml
-
-# link the session file to .tmuxinator.yml
-ln session_dry.yml .tmuxinator.yml
+export TMUX_SESSION_NAME=dry
+export TMUX_SOCKET_NAME=mrs
 
 # start tmuxinator
-tmuxinator
+tmuxinator start -p ./session_dry.yml
+
+# if we are not in tmux
+if [ -z $TMUX ]; then
+
+  # just attach to the session
+  tmux -L $TMUX_SOCKET_NAME a -t $TMUX_SESSION_NAME
+
+# if we are in tmux
+else
+
+  # switch to the newly-started session
+  tmux detach-client -E "tmux -L $TMUX_SOCKET_NAME a -t $TMUX_SESSION_NAME" 
+
+fi
